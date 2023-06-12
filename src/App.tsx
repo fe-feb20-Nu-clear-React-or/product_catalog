baf4e9238a5c480717ea1bb597c6b654e8b95f00
Mikołaj Ostrowski <120560813+MikiOOOO@users.noreply.github.com>
2023-06-12 14:21:26 +0100

    solution (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.scss';
+import { NavBar } from './components/NavBar';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import { Phones } from './components/Phones';
 
 function App() {
   return (
-    <div className="App">
-      Init app
-    </div>
+      <div className="App">
+        <NavBar />
+        
+        <Routes>
+          <Route path="/home" element={<h1>home</h1>}/>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/phones" element={<Phones />}/>
+          <Route path="/tablets" element={<h1>tablets</h1>}/>
+          <Route path="/accessories" element={<h1>accessories</h1>}/>
+        </Routes>
+      </div>
   );
 }
 
