8216e4480a54cd4938c653ef4d32da50b9768c42
Amadeusz Michalowski <amadeusz.michalowski@gmail.com>
2023-06-13 16:28:23 +0200

    back-to-top fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,6 @@
 import './Footer.scss';
 import niceGadgets from '../../icons/NiceGadgets.svg'
 import upArrow from '../../icons/Chevron (Arrow Up).svg'
-import { NavLink } from 'react-router-dom'
 
 export const Footer = () => (
   <footer className="footer">
@@ -21,13 +20,12 @@ export const Footer = () => (
 
     <div className='footer__back-to-top'>
       <span className='footer__back-to-top back-to-top__text'>Back to top</span>
-      <a 
-        href='#/home'
+      <div
         className='footer__back-to-top back-to-top__button'
         onClick={() => document.documentElement.scrollTop = 0}
       >
           <img src={upArrow} alt="back-to-top"/>
-      </a>
+      </div>
     </div>
   </footer>
 );
