84923d43269333ed4f5529ad21ad07859af0fa48
Mikołaj Ostrowski <120560813+MikiOOOO@users.noreply.github.com>
2023-06-13 16:45:50 +0100

    Home page layout (#16)
    * solution
    
    * home page mobile ver
    
    ---------
    
    Co-authored-by: Barbara Jeziorek <bjeziorek@users.noreply.github.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import './Footer.scss';
 import niceGadgets from '../../icons/NiceGadgets.svg'
 import upArrow from '../../icons/Chevron (Arrow Up).svg'
+import { NavLink } from 'react-router-dom'
 
 export const Footer = () => (
   <footer className="footer">
@@ -20,12 +21,17 @@ export const Footer = () => (
 
     <div className='footer__back-to-top'>
       <span className='footer__back-to-top back-to-top__text'>Back to top</span>
-      <div
+
+      <NavLink 
+        to="/home"
+      >
+        <div
         className='footer__back-to-top back-to-top__button'
         onClick={() => document.documentElement.scrollTop = 0}
-      >
-          <img src={upArrow} alt="back-to-top"/>
-      </div>
+        >
+          <img src={upArrow} alt="back-to-top"/> 
+        </div>
+      </NavLink>
     </div>
   </footer>
 );
