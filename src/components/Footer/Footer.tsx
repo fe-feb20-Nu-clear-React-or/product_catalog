feb37300fca605653941f05917b034edcbbe293e
Mikołaj <ostermi@wp.pl>
2023-06-13 17:33:46 +0100

    fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,7 +20,6 @@ export const Footer = () => (
     </ul>
 
     <div className='footer__back-to-top'>
-<<<<<<< HEAD
       <p className='footer__back-to-top-text'>Back to top</p>
       <NavLink 
         to="/home"
@@ -32,19 +31,6 @@ export const Footer = () => (
             alt="back-to-top"
             className="footer__back-to-top-image"
           />
-=======
-      <span className='footer__back-to-top back-to-top__text'>Back to top</span>
-
-      <NavLink 
-        to="/home"
-      >
-        <div
-        className='footer__back-to-top back-to-top__button'
-        onClick={() => document.documentElement.scrollTop = 0}
-        >
-          <img src={upArrow} alt="back-to-top"/> 
-        </div>
->>>>>>> origin/homePageLayout
       </NavLink>
     </div>
   </footer>
