6323716bc3054b5b0014b944ace04205d3b84335
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-06-13 12:39:25 +0200

    pagination WIP

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import ApiDataContext from "../ApiDataContext";
 import { Product } from "../types/Product";
+import { List } from "./List";
 
 export const Home = () => {
 
-  const data = useContext<Product[]>(ApiDataContext);
+ // const data = useContext<Product[]>(ApiDataContext);
 
 return (
   <div className="home">
     Home
-    {data.map((el:Product)=><p key={el.id}>{el.name}</p>)}
+    <List/>
+    {/* {data.map((el:Product)=><p key={el.id}>{el.name}</p>)} */}
   </div>
 )};
