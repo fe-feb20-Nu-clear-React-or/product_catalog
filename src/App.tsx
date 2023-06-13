08a066ec6516251cfc0c88f880f6b43a0bb82a22
suaby514 <suaby1725@gmail.com>
2023-06-13 09:57:36 +0200

    Router and back-to-home link

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
 import './App.scss';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
-import {Routes, Route, Navigate} from 'react-router-dom';
 import { Phones } from './components/Phones';
+import { NotFoundPage } from './components/NotFoundPage';
 
 function App() {
   return (
-      <div className="App">
-        <NavBar />
-        
-        <Routes>
-          <Route path="/home" element={<h1>home</h1>}/>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/phones" element={<Phones />}/>
-          <Route path="/tablets" element={<h1>tablets</h1>}/>
-          <Route path="/accessories" element={<h1>accessories</h1>}/>
-        </Routes>
-      </div>
+    <div className="App">
+      <NavBar />
+
+      <Routes>
+        <Route path="/home" element={<h1>home</h1>} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/phones" element={<Phones />} />
+        <Route path="/tablets" element={<h1>tablets</h1>} />
+        <Route path="/accessories" element={<h1>accessories</h1>} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
   );
 }
 
