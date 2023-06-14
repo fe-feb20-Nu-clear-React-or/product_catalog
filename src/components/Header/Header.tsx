cc4964e630c35305c0be21fdfa423ba86a6c411b
Mikołaj <ostermi@wp.pl>
2023-06-14 18:08:25 +0100

    responsive layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,51 @@
 // eslint-disable-next-line max-len
 import { Resolution } from '../../types/Resolution';
+import headerImageDesktop from
+  '../../photos/home-page-header-image-desktop.png';
+import headerImageMobile from '../../photos/home-page-header-image-mobile.png';
 
-interface HeaderProps {
-  resolution: Resolution,
-}
-
+import React from 'react';
 import './Header.scss';
 
-export const Header:React.FC<HeaderProps> = ({resolution}) => {
+interface HeaderProps {
+  resolution: Resolution;
+}
+
+export const Header: React.FC<HeaderProps> = ({ resolution }) => {
   return (
     <section className="header">
-      <h1 className="header__title">Welcome to Nice<br></br> Gadgets store!</h1>
+      <h1 className="header__title">
+        Welcome to Nice
+        {resolution !== Resolution.DESKTOP
+          ? <br /> : <>&nbsp;</>} Gadgets store!
+      </h1>
       <div className="header__image-wrapper">
         {[Resolution.TABLET, Resolution.DESKTOP].includes(resolution) ? (
-          <><button
-            className="carousel__switch-button
-            carousel__switch-button--desktop"
-          >
-            {'<'}
-          </button>
-          <div
-            className="header__image-container"
-          >
-          </div>
-          <button
-            className="carousel__switch-button
-        carousel__switch-button--desktop"
-          >
-            {'>'}
-          </button></>
-
+          <>
+            <button
+              className="carousel__switch-button
+              carousel__switch-button--desktop"
+            >
+              {'<'}
+            </button>
+            <img
+              className="header__image"
+              src={headerImageDesktop}
+              alt="iphone header image"
+            />
+            <button
+              className="carousel__switch-button
+              carousel__switch-button--desktop"
+            >
+              {'>'}
+            </button>
+          </>
         ) : (
-          <div
-            className="header__image-container"
-          >
-          </div>
+          <img
+            className="header__image"
+            src={headerImageMobile}
+            alt="iphone header image"
+          />
         )}
       </div>
       <span className="header__navigation">
