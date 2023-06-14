bcc0b3d29ca912ee19bd5b290c82fdc1cd34f995
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-06-14 09:04:54 +0200

    fixes from linter

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
 import { useContext, useEffect, useState } from "react";
 import { Card } from "../Card/Card";
 import './Carousel.scss';
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useContext, useEffect, useState } from "react";
 import ApiDataContext from "../ApiDataContext";
 import { Pagination } from "./Pagination";
