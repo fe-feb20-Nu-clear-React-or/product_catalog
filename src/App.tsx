fa7c612c78a3afe1add331f34c385e65a175a3b1
Mikołaj <ostermi@wp.pl>
2023-06-14 11:31:10 +0100

    resolution change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { Navbar } from './components/Navbar/Navbar';
 import {Routes, Route, Navigate} from 'react-router-dom';
@@ -7,19 +7,43 @@ import { BurgerMenu } from './components/BurgerMenu/BurgerMenu';
 import { Footer } from './components/Footer/Footer';
 import {ApiDataProvider} from './ApiDataContext';
 import { Home } from './components//Home/Home';
+import { Resolution } from './types/Resolution';
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [resolution, setResolution] = useState<Resolution>()
 
   const handlesetIsMenuOpen = (isOpen: boolean) => {
     setIsMenuOpen(isOpen);
   };
 
+  useEffect(()=> {
+    const handleResolutionSet = () => {
+      if (window.innerWidth < 640) {
+        setResolution(Resolution.MOBILE);
+      } else if (window.innerWidth < 1200) {
+        setResolution(Resolution.TABLET);
+      } else {
+        setResolution(Resolution.DESKTOP);
+      }
+    }
+
+    handleResolutionSet();
+
+    window.addEventListener('resize', handleResolutionSet);
+
+    return () => {
+      window.removeEventListener('resize', handleResolutionSet);
+    }
+  }, []);
+
+  console.log(resolution);
+
   return (
       <div className="App">
         <ApiDataProvider>
         <Navbar isMenuOpen={isMenuOpen} handleSetIsMenuOpen={handlesetIsMenuOpen} />
-        {isMenuOpen 
+        {isMenuOpen
         ? (<BurgerMenu handleSetIsMenuOpen={handlesetIsMenuOpen} />)
         : (
           <>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,13 +7,16 @@ import { useEffect, useState } from 'react';
 
 export const Header = () => {
     const [headerImageSrc, setHeaderImageSource] = useState('');
+    const [isDesktopResolution, setIsDesktopResolution] = useState(false);
 
     useEffect(() => {
       const handleResize = () => {
         if (window.innerWidth >= 640) {
           setHeaderImageSource(headerImageDesktop);
+          setIsDesktopResolution(true);
         } else {
             setHeaderImageSource(headerImageMobile)
+            setIsDesktopResolution(false);
         }
       };
 
@@ -29,11 +32,27 @@ export const Header = () => {
     return (
         <section className="header">
             <h1 className="header__title">Welcome to Nice<br></br> Gadgets store!</h1>
-            <img
-                className="header__image"
-                alt="Iphone 14 Pro"
-                src={headerImageSrc}
-            />
+            {isDesktopResolution ? (
+                <div className="header__image-desktop-container">
+                  <button className="carousel__switch-button carousel__switch-button--desktop">
+                    {'<'}
+                  </button>
+                  <img
+                    className="header__image header__image--desktop"
+                    alt="Iphone 14 Pro"
+                    src={headerImageSrc}
+                  />
+                  <button className="carousel__switch-button carousel__switch-button--desktop">
+                    {'>'}
+                  </button>
+                </div>
+              ) : (
+                <img
+                  className="header__image"
+                  alt="Iphone 14 Pro"
+                  src={headerImageSrc}
+                />
+              )}
             <span className="header__navigation">
                 <div className="header__navigation-bar header__navigation-bar--active"></div>
                 <div className="header__navigation-bar"></div>
