45660f163818573aacc3d80a8e59edcb793c0748
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-06-15 16:07:19 +0200

    slider

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 // eslint-disable-next-line max-len
 import { Resolution } from '../../types/Resolution';
-import headerImageDesktop from
-  '../../photos/home-page-header-image-desktop.png';
+import headerImageDesktop1 from 
+  '../../photos/home-page-header-image-desktop1.png';
+import headerImageDesktop2 from 
+  '../../photos/home-page-header-image-desktop2.png';
+import headerImageDesktop3 from 
+  '../../photos/home-page-header-image-desktop3.png';
 import headerImageMobile from '../../photos/home-page-header-image-mobile.png';
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
 
 interface HeaderProps {
@@ -12,6 +16,19 @@ interface HeaderProps {
 }
 
 export const Header: React.FC<HeaderProps> = ({ resolution }) => {
+  const slides = [headerImageDesktop1,headerImageDesktop2,headerImageDesktop3];
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  
+
+  const handleSlideForward=()=>{
+    setCurrentSlideIndex(prev => (prev+1)%slides.length);
+  };
+
+  const handleSlideBack=()=>{
+    setCurrentSlideIndex(prev => (prev-1+slides.length)%slides.length);
+  };
+
   return (
     <section className="header">
       <h1 className="header__title">
@@ -25,17 +42,19 @@ export const Header: React.FC<HeaderProps> = ({ resolution }) => {
             <button
               className="carousel__switch-button
               carousel__switch-button--desktop"
+              onClick={handleSlideBack}
             >
               {'<'}
             </button>
             <img
               className="header__image"
-              src={headerImageDesktop}
+              src={slides[currentSlideIndex]}
               alt="iphone header image"
             />
             <button
               className="carousel__switch-button
               carousel__switch-button--desktop"
+              onClick={handleSlideForward}
             >
               {'>'}
             </button>
@@ -49,10 +68,9 @@ export const Header: React.FC<HeaderProps> = ({ resolution }) => {
         )}
       </div>
       <span className="header__navigation">
-        <div className="header__navigation-bar
-        header__navigation-bar--active"></div>
-        <div className="header__navigation-bar"></div>
-        <div className="header__navigation-bar"></div>
+        <div className={`header__navigation-bar${currentSlideIndex===0?' header__navigation-bar--active':''}`}></div>
+        <div className={`header__navigation-bar${currentSlideIndex===1?' header__navigation-bar--active':''}`}></div>
+        <div className={`header__navigation-bar${currentSlideIndex===2?' header__navigation-bar--active':''}`}></div>
       </span>
     </section>
   );
