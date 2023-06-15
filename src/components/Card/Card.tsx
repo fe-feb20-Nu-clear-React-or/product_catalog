05e93511aa3265bc899b639f6da9f721e9cadd3c
Mikołaj <ostermi@wp.pl>
2023-06-15 10:26:19 +0100

    adding phones images

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,6 @@
 import { Product } from '../../types/Product';
 import './Card.scss';
-import placeholder from '../../img/phones/apple-iphone-11/black/01.jpg';
+// import placeholder from '../../img/phones/apple-iphone-11/green/01.jpg';
 
 interface Props {
   product:Product;
@@ -9,16 +9,16 @@ interface Props {
 export const Card:React.FC<Props> = ({product}) => {
   return (
     <section className="card">
-      <img 
+      <img
         className="card__photo"
-        src={placeholder}
+        src={require(`../../${product.image}`)}
         alt={product.name}
       ></img>
       <h1 className="card__name">{product.name}</h1>
       <div className="card__price">${product.price}</div>
-      
+
       <div className='card__divider'></div>
-     
+
       <div className="card__info">
         <div className="card__info--details">Screen:</div>
         <div className="card__info--value">{product.screen}</div>
@@ -28,18 +28,18 @@ export const Card:React.FC<Props> = ({product}) => {
         <div className="card__info--details">Capacity:</div>
         <div className="card__info--value">{product.capacity}</div>
       </div>
-        
+
       <div className="card__info">
         <div className="card__info--details">RAM:</div>
         <div className="card__info--value">{product.ram}</div>
       </div>
-  
+
       <div className='card__buttons'>
         <a className="card__buttons--buy">Buy</a>
         <a className="card__buttons--fav">3</a>
       </div>
-      
-  
+
+
     </section>
   );
 };
