90204249d6813abe61b4dd8347d330912783c757
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-06-16 14:38:01 +0200

    missing under construction pages + not found styles

---
+++ src/components/Tablets/Tablets.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import './Tablets.scss';
+
+export const Tablets = () => (
+  <>
+    <h1 className='tablets__title'>Tablets (soon)</h1>
+    <Link to="/home" className="tablets__return">Return to&nbsp;
+      <span className='tablets__link'>Home</span>
+    </Link>
+  </>
+);
